3e3dc1751ef38ae7f56995bfaf8ff1586c7a97c6
Woody Gilk <woody.gilk@gmail.com>
2015-12-06 14:57:55 -0600

    Add "Fork Me" banner

--- templates/layout.php
+++ templates/layout.php
@@ -19,12 +19,12 @@
         <div class="header clearfix">
             <nav>
                 <ul class="nav nav-pills pull-right">
-                    <?php if ($this->is_logged_in()): ?>
+                <?php if ($this->is_logged_in()): ?>
                     <li role="presentation"><a href="/">Import</a></li>
                     <li role="presentation"><a href="/logout" id="logout">Logout</a></li>
-                    <?php else: ?>
+                <?php else: ?>
                     <li role="presentation"><a href="/login">Login</a></li>
-                    <?php endif ?>
+                <?php endif ?>
                 </ul>
             </nav>
             <h3 class="text-muted">RadioTide</h3>
@@ -43,6 +43,8 @@
             </small></p>
         </div>
     </div>
+
+    <a href="https://github.com/shadowhand/radiotide"><img style="position: absolute; top: 0; right: 0; border: 0;" src="https://camo.githubusercontent.com/38ef81f8aca64bb9a64448d0d70f1308ef5341ab/68747470733a2f2f73332e616d617a6f6e6177732e636f6d2f6769746875622f726962626f6e732f666f726b6d655f72696768745f6461726b626c75655f3132313632312e706e67" alt="Fork me on GitHub" data-canonical-src="https://s3.amazonaws.com/github/ribbons/forkme_right_darkblue_121621.png"></a>
 
     <script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
     <script src="//maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js" integrity="sha384-0mSbJDEHialfmuBBQP6A4Qrprq5OVfW37PRR3j5ELqxss1yVqOtnepnHVP9aJ7xS" crossorigin="anonymous"></script>
